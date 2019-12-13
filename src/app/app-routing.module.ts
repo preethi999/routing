84299b39522390352a0120d20356060135f78677
preethi999 <dvnsdpreethi@gmail.com>
2019-12-13 13:26:24 +0530

    routingmodules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ProductComponent } from './pages/product/product.component';
+import { DescriptionComponent } from './pages/description/description.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { CheckoutComponent } from './pages/checkout/checkout.component';
+import { NavigationComponent } from './pages/navigation/navigation.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:HomeComponent},
+  {path:"about",component:AboutComponent},
+  {path:"product",component:ProductComponent},
+  {path:"description",component:DescriptionComponent},
+  {path:"cart",component:CartComponent},
+  {path:"checkout",component:CheckoutComponent},
+  {path:"navigation",component:NavigationComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './pages/about/about.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { ProductComponent } from './pages/product/product.component';
+import { DescriptionComponent } from './pages/description/description.component';
+import { CheckoutComponent } from './pages/checkout/checkout.component';
+import { NavigationComponent } from './pages/navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    HomeComponent,
+    CartComponent,
+    ProductComponent,
+    DescriptionComponent,
+    CheckoutComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
